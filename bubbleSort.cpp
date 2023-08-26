a97156b7761c77a93d0ac8471f8561e6bb1ff90e
SHUBHAM SINHA <94913494+shubhamsinha503@users.noreply.github.com>
2023-08-27 00:41:26 +0530

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include <climits>
+#include<math.h>
+using namespace std;
+
+void PrintArray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        cout<< arr[i]<< " ";
+    }
+    cout<<endl;
+}
+
+void BubbleSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int arr[10]={9,8,7,6,5,4,3,2,1,0};
+
+    PrintArray(arr,10);
+    BubbleSort(arr,10);
+    PrintArray(arr,10);
+
+}
